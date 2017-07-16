8713478b0c5f2a26d4847f6303d76bb5482742bc
Mike Tarkington <tarkmike@gmail.com>
2017-07-16 15:06:25 -0700

    setup redux state management for email and password input fields

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,9 +1,21 @@
-import { EMAIL_CHANGED } from '../actions/types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED
+ } from '../actions/types';
 
-const INITIAL_STATE = { email: '' };
+const INITIAL_STATE = {
+  email: '',
+  password: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMAIL_CHANGED:
+    // make a new object, take all existing properties of state and include them in that object, and then define the property email (which is empty by default) and then overwrite it with a value of the action payload... then the state will be updated if the comparison of this new object is true against the old state... use mapStateToProps in LoginForm to transer this new state to props elsewhere
+      return { ...state, email: action.payload };
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload };
+
     default:
       return state;
   }
